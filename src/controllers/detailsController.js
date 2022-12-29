c11d6afb359e42e83681253359bee1e98c67cdef
TU2508 <hvt25082001a@gmail.com>
2022-12-29 23:24:42 +0700

    29

--- src/controllers/detailsController.js
+++ src/controllers/detailsController.js
@@ -3,7 +3,7 @@ import pool from "../configs/connectDB";
 const getdetailsController = async (req,res) => {
     let id = req.params.id
     let product = await pool.execute('select * from products where id = ?',[id])
-    return res.render("details.ejs", {product: JSON.stringify( product[0])})
+    return res.render("details.ejs", {product:  product[0]})
 }
 
 export default {
